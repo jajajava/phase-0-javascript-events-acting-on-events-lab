9dc688aead01c5a0003eedfe386f3ef50462d81f
David <davidstepanov@yahoo.com>
2022-08-17 22:55:59 -0400

    commit tree 02ba7f979312afb37b8df6b62625b5efb0be3bc8

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.bottom = "100px"
+
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    dodger.style.left = `${left - 1}px`;
+  }
+  });
+
+
+  function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10)
+
+    dodger.style.right = `${left + 1}`
+  }
+
+
+
+ document.addEventListener("keydown",function(e){
+  if(e.key === "ArrowRight"){
+    moveDodgerRight
+  }
+ })
+
